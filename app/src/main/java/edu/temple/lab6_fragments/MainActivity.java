f57d350f389568bf4c241024baa1bff8e5f6c13f
Nick <nshupinski@gmail.com>
2020-03-16 17:58:34 -0400

    Initial commit. A palette fragment and canvas fragment were created including their java classes and xml files. The palette fragment was added to the main activity.

---
+++ app/src/main/java/edu/temple/lab6_fragments/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.temple.lab6_fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
